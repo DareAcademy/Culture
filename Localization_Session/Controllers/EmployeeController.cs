f87b7b5941b136e34df0ca38a1d564ad94442b37
DareAcademy <88663592+DareAcademy@users.noreply.github.com>
2022-06-20 14:33:48 +0300

    add files

---
+++ Localization_Session/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Localization_Session.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IStringLocalizer<EmployeeController> _localizer;
+
+        public EmployeeController(IStringLocalizer<EmployeeController> localizer)
+        {
+            _localizer = localizer;
+        }
+        public IActionResult NewEmployee()
+        {
+            ViewData["message"]= _localizer["test"];
+            return View();
+        }
+
+        public IActionResult EmployeeList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Localization_Session/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Localization_Session.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "NameRequired")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "PhoneRequired")]
+        public string Phone { get; set; }
+
+    }
+}
